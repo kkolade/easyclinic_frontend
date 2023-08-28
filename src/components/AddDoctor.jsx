06693a603cc0a9508a61ef0d491ce05dbd115461
Kola Kolade <kolakolade@gmail.com>
2023-08-28 20:35:02 +0100

    Add components folder | Add necessary components and minimal template

---
+++ src/components/AddDoctor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddDoctor = () => (
+  <div>
+    <h2>Add Doctor</h2>
+  </div>
+);
+
+export default AddDoctor;
---
+++ src/components/Doctors.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Doctors = () => (
+  <div>
+    <h2>Home Page</h2>
+  </div>
+);
+
+export default Doctors;
