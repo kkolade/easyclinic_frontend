d4e4cfdbbc39f658a34697c0bf8a4e02c61eadea
Kola Kolade <kolakolade@gmail.com>
2023-08-28 22:31:49 +0100

    Wrap App component with provider and make store available to App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider withNormalizeCSS withGlobalStyles theme={theme}>
-      <App />
-    </MantineProvider>
+    <Provider store={store}>
+      <MantineProvider withNormalizeCSS withGlobalStyles theme={theme}>
+        <App />
+      </MantineProvider>
+    </Provider>
   </React.StrictMode>,
 );
