91c95a5a21e8b5ce4c65087cb3a228ecc0cd315d
Rashed Arman <rashedarman21@gmail.com>
2023-08-30 20:43:48 +0600

    create 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,33 @@
+import {
+  Button,
+  Center,
+  Container,
+  Flex,
+  Text,
+  Title,
+  rem,
+} from '@mantine/core';
+import { IconChevronLeft } from '@tabler/icons-react';
+
 const NotFoundPage = () => (
-  <div>
-    <h2>404: Page Not Found</h2>
-    <p>Please check the URL.</p>
-  </div>
+  <Container fluid>
+    <Center h="100vh">
+      <Flex direction="column" justify="center" align="center" rowGap="sm">
+        <Title order={2} fw="lighter">
+          Not Found (404)
+        </Title>
+        <Text c="dimmed" fz={rem(17)}>
+          Sorry, the page you are looking for does not exist.
+        </Text>
+        <Button component="a" href="/" sx={{ marginTop: rem(16) }}>
+          <IconChevronLeft size={rem(20)} />
+          <Text fz={rem(15)} sx={{ marginLeft: rem(8) }}>
+            Back to home
+          </Text>
+        </Button>
+      </Flex>
+    </Center>
+  </Container>
 );
 
 export default NotFoundPage;
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { rem } from '@mantine/core';
+
 /**
  * @type {import ('@mantine/core').MantineThemeOverride}
  */
@@ -5,6 +7,22 @@ const themeOverride = {
   fontFamily: 'Roboto, sans-serif',
   headings: {
     fontFamily: 'Alfa Slab One, cursive',
+  },
+  components: {
+    Button: {
+      styles: {
+        root: {
+          padding: `0 ${rem(16)}`,
+        },
+      },
+    },
+    Text: {
+      styles: {
+        root: {
+          fontWeight: 'normal',
+        },
+      },
+    },
   },
 };
 
