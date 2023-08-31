4f1287037a03120ff00fcb661888c13fcd2fd8e5
Rashed Arman <rashedarman21@gmail.com>
2023-08-31 19:34:00 +0600

    unit tests for NotFoundPage

--- src/__tests__/pages/NotFoundPage.test.jsx
+++ src/__tests__/pages/NotFoundPage.test.jsx
@@ -1,9 +1,35 @@
+import '@testing-library/jest-dom';
+
+import userEvent from '@testing-library/user-event';
 import NotFoundPage from 'pages/NotFoundPage';
 import { render } from 'test-utils';
 
 describe('NotFoundPage', () => {
   it('should render correctly', () => {
-    const { asFragment } = render(<NotFoundPage />);
+    const { asFragment, getByText } = render(<NotFoundPage />);
+
+    // check if title is rendered
+    const titleElement = getByText(/Not Found \(404\)/i);
+    expect(titleElement).toBeInTheDocument();
+
+    // check if text is rendered
+    const textElement = getByText(/Sorry, the page you are looking for does not exist/i);
+    expect(textElement).toBeInTheDocument();
+
+    // check if back to home button is rendered
+    const buttonElement = getByText(/Back to home/i);
+    expect(buttonElement.closest('a')).toHaveAttribute('href', '/');
+
+    // check if snapshot matches
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should redirect to home page when back to home button is clicked', async () => {
+    const { getByText } = render(<NotFoundPage />);
+
+    const buttonElement = getByText(/Back to home/i);
+    userEvent.click(buttonElement);
+
+    expect(window.location.pathname).toBe('/');
   });
 });
