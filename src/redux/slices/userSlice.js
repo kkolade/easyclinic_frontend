7f18d8f1922aa95c1fb1b57369020f3039cc16e1
Rashed Arman <rashedarman21@gmail.com>
2023-09-02 20:38:56 +0600

    create initial userReducer

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,73 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+import { API_BASE_URL as BASE_URL } from 'utils/constants';
+
+export const userLogin = createAsyncThunk(
+  'user/login',
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/login`,
+        { username, password },
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+      return {
+        user: response.data,
+        jwt: response.headers.authorization,
+      };
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+    jwt: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    loadUserFromLocalStorage: (state) => {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const jwt = localStorage.getItem('jwt');
+      if (user && jwt) {
+        state.user = user;
+        state.jwt = jwt;
+      }
+    },
+    userLogout: (state) => {
+      state.user = null;
+      state.jwt = null;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(userLogin.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(userLogin.fulfilled, (state, action) => {
+        localStorage.setItem('user', JSON.stringify(action.payload));
+        state.loading = false;
+        state.error = null;
+        state.user = action.payload.user;
+        state.jwt = action.payload.jwt;
+      })
+      .addCase(userLogin.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { loadUserFromLocalStorage, userLogout } = userSlice.actions;
+
+export default userSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
-    placeholderReducer: () => ({}),
+    user: userReducer,
   },
 });
+
+export const selectUser = (state) => state.user.user;
+export const selectJWT = (state) => state.user.jwt;
+export const selectUserLoading = (state) => state.user.loading;
+export const selectUserError = (state) => state.user.error;
 
 export default store;
