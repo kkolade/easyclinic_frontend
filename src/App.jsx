fe5c94cfd7fbc33c84a025bdcbdd647d3aed3066
Rashed Arman <rashedarman21@gmail.com>
2023-09-03 12:52:20 +0600

    create ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Route, Routes, useLocation, useNavigate,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
+import ProtectedRoute from './components/ProtectedRoutes';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import SigninPage from './pages/SigninPage';
@@ -13,8 +12,6 @@ import { selectUser } from './redux/store';
 
 const App = () => {
   const dispatch = useDispatch();
-  const location = useLocation();
-  const navigate = useNavigate();
 
   const user = useSelector(selectUser);
 
@@ -22,22 +19,13 @@ const App = () => {
     dispatch(loadUserFromLocalStorage());
   }, [dispatch]);
 
-  // todo: fix component flash (using protected routes?)
-  useEffect(() => {
-    if (user) {
-      if (location.pathname === '/signin' || location.pathname === '/signup') {
-        navigate('/');
-      }
-    } else if (location.pathname !== '/signin' && location.pathname !== '/signup') {
-      navigate('/signin');
-    }
-  }, [user, location, navigate]);
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/signin" element={<SigninPage />} />
-      <Route path="/signup" element={<SignupPage />} />
+      <Route element={<ProtectedRoute isAllowed={!user} />}>
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
