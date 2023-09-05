5625f49bc3348653a2e964292509cf9c8b46fc25
Rashed Arman <rashedarman21@gmail.com>
2023-09-05 19:52:31 +0600

    adjust details page spacing

--- src/components/DoctorDetails.jsx
+++ src/components/DoctorDetails.jsx
@@ -1,5 +1,5 @@
 import {
-  Avatar, SimpleGrid, Text, Title, createStyles,
+  Avatar, Badge, Box, SimpleGrid, Space, Text, Title, createStyles,
 } from '@mantine/core';
 import PropTypes from 'prop-types';
 
@@ -7,7 +7,7 @@ const useStyles = createStyles((theme) => ({
   table: {
     fontWeight: 600,
     width: '100%',
-    margin: `${theme.spacing.xl} auto`,
+    margin: `${theme.spacing.xs} auto`,
     td: {
       display: 'flex',
       justifyContent: 'space-between',
@@ -64,21 +64,18 @@ const DoctorDetails = ({ data }) => (
     cols={2}
     breakpoints={[{ maxWidth: 'md', cols: 1 }]}
     spacing="xs"
-    sx={{
-      height: '100%',
-      placeItems: 'center',
-    }}
+    sx={{ height: '100%', placeItems: 'center' }}
   >
     <div>
       <Avatar src={data.photo} alt={data.name} size={250} radius="50%" />
     </div>
-    <div>
-      <Title align="right">{data.name}</Title>
-      <Text c="dark.3" align="right">
-        {data.bio}
-      </Text>
+    <Box sx={{ textAlign: 'right' }}>
+      <Title>{data.name}</Title>
+      <Badge>{data.specialty?.name || 'General Practitioner'}</Badge>
+      <Space h="sm" />
+      <Text c="dark.3">{data.bio}</Text>
       <DoctorTable data={data} />
-    </div>
+    </Box>
   </SimpleGrid>
 );
 
@@ -87,6 +84,9 @@ const DoctorDataProp = {
     name: PropTypes.string.isRequired,
     photo: PropTypes.string.isRequired,
     bio: PropTypes.string.isRequired,
+    specialty: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
     user: PropTypes.shape({
       phone_number: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
