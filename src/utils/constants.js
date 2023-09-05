0dc97b810df62655e4427d371bf23a4456c6cc82
Rashed Arman <rashedarman21@gmail.com>
2023-09-05 22:44:32 +0600

    Revert "change to live api domain"
    This reverts commit 3d80f9997bc3373e1207e9a2bd772abc3e4bde0e.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const LOCAL_STORAGE_USER_KEY = 'user';
 export const LOCAL_STORAGE_JWT_KEY = 'jwt';
 
-export const API_BASE_URL = 'https://rails-xdb0.onrender.com';
+export const API_BASE_URL = 'http://localhost:3000';
 export const API_URL = `${API_BASE_URL}/api/v1`;
